c3ea3764c8310670999510fdddbc13ee3350c9c5
geniuus <cross_man@naver.com>
2022-11-16 19:34:49 +0900

    start, main 페이지 버튼으로 연결

---
+++ shoppingmall/src/components/main/main.js
@@ -0,0 +1,7 @@
+const Main = () => (
+    <div>
+    <h1> 메인 페이지 입니당.</h1>
+    </div>
+);
+
+export default Main;
---
+++ shoppingmall/src/components/start/start.js
@@ -0,0 +1,9 @@
+import {Link} from "react-router-dom";
+
+const Start = () => (
+    <div>
+        <button><Link to="main">Shop now</Link></button>
+    </div>
+);
+
+export default Start;
