f21937f75099caa6935221967a5f3e55861fa992
Jeonghan Kim <gtrew9606@naver.com>
2022-11-17 20:35:25 +0900

    json파일 수정완료 + header.js 하는 중

--- shoppingmall/src/components/header/header.js
+++ shoppingmall/src/components/header/header.js
@@ -1,17 +1,44 @@
 import "./header.css"
 import {Fragment} from 'react';
+import {Product} from "../product/product"
 
-const Header = () => (
-    <Fragment className="header">
+const Header = ({convertMenu, products, setProducts}) => {
+    const sortProduct = (type) => {
+        if (type === "All") {
+        const newProduct = [...products];
+        newProduct.sort((a, b) => a.id - b.id);
+        setProducts(newProduct);
+        } else if (type === "Outer") {
+        const newProduct = [...products];
+        newProduct.sort((a, b) => a.price - b.price);
+        setProducts(newProduct);
+        } else if (type === "Top") {
+        const newProduct = [...products];
+        newProduct.sort((a, b) => b.price - a.price);
+        setProducts(newProduct);
+        } else if (type === "Bottom") {
+        const newProduct = [...products];
+        newProduct.sort((a, b) => b.price - a.price);
+        setProducts(newProduct);
+        } else if (type === "Acc") {
+        const newProduct = [...products];
+        newProduct.sort((a, b) => b.price - a.price);
+        setProducts(newProduct);
+        } 
+    };
+
+    return (
+    <>
+    <div className="header">
     
     <h1 id="main_title">Exception;</h1>
         <ul className="menu_header">
             <ul className="category">
-                <li>All</li>
-                <li>Outer</li>
-                <li>Top</li>
-                <li>Bottom</li>
-                <li>Acc</li>
+                <li onClick={() => sortProduct("All")}>All</li>
+                <li onClick={() => sortProduct("Outer")}>Outer</li>
+                <li onClick={() => sortProduct("Top")}>Top</li>
+                <li onClick={() => sortProduct("Bottom")}>Bottom</li>
+                <li onClick={() => sortProduct("Acc")}>Acc</li>
             </ul>
 
             <ul className="personal">
@@ -22,7 +49,9 @@ const Header = () => (
         
 
     
-    </Fragment>
-);
+    </div>
+    </>
+    );
+};
 
 export default Header;
