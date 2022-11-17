41595334cc6ba956717232f4b061ebe72ae4489c
geniuus <cross_man@naver.com>
2022-11-17 20:14:44 +0900

    products.json 경로명 수정

--- shoppingmall/src/components/main/main.js
+++ shoppingmall/src/components/main/main.js
@@ -2,7 +2,7 @@ import "./main.css"
 import {Fragment} from 'react';
 import Header from "../header/header";
 import {Product} from "../product/product";
-import productList from "../../../public/products/products";
+import productList from "../../products/products";
 
 const Main = ({products, setProducts}) => {
     console.log(productList)
