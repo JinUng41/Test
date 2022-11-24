e4bc0ade58452d9a17f09805dcdaef709f3175a9
Jeonghan Kim <gtrew9606@naver.com>
2022-11-24 19:41:12 +0900

    메뉴 구현, underline효과, 홈화면 바로가기 구현

--- shoppingmall/src/components/header/header.js
+++ shoppingmall/src/components/header/header.js
@@ -4,53 +4,83 @@ import { useEffect } from "react";
 //import {Product} from "../product/product"
 import { getProducts } from "../../service/fetcher";
 
-const Header = ({convertMenu, products, setProducts}) => {
+const Header = ({products, setProducts}) => {
+    var newProduct=[];
+    getProducts().then((data) => {
+        newProduct = (data.data.products);
+    });
+    // let newProduct = [...products];
+    let result;
+    console.log(newProduct);
     let sortProduct = (type) => {
-        if (type === "All") {
-            let newProduct = [...products];
-            setProducts(newProduct);
-        } 
-        else if (type === "Outer") {
-            let newProduct = [...products];
-            let result = newProduct.filter(product => product.category==="Outer")
-            setProducts(result);
-            console.log(result);
-            console.log(newProduct);
-        } 
-        else if (type === "Top") {
-            let newProduct = [...products];
-            let result = newProduct.filter(product => product.category==="Top")
-            setProducts(result);
-            console.log(result);
-            console.log(newProduct);
-            console.log(...products);
 
-        } 
-        else if (type === "Bottom") {
-            const newProduct = [...products];
-            const result = newProduct.filter(product => product.category==="Bottom")
-            setProducts(result);
-            console.log(result);
-        } 
-        else if (type === "Acc") {
-            const newProduct = [...products];
-            const result = newProduct.filter(product => product.category.includes("Acc"))
-            setProducts(result);
-            console.log(result);
-            console.log(newProduct);
-            console.log(...products);
-        } 
-     };
 
-     useEffect(() => {
+        switch(type){
+            case "All":
+                setProducts(newProduct);
+                break;
+            case "Outer":
+                result = newProduct.filter(product => product.category==="Outer")
+                setProducts(result);
+                break;
+            case "Top":
+                result = newProduct.filter(product => product.category==="Top")
+                setProducts(result);
+                break;
+            case "Bottom":
+                result = newProduct.filter(product => product.category==="Bottom")
+                setProducts(result);
+                break;
+            case "Acc":
+                result = newProduct.filter(product => product.category.includes("Acc"))
+                setProducts(result);
+                break;
+        }
+
+
+        // if (type === "All") {
+        //     setProducts(newProduct);
+        // } 
+        // else if (type === "Outer") {
+        //     setProducts(newProduct);
+        //     result = newProduct.filter(product => product.category==="Outer")
+        //     setProducts(result);
+        //     console.log(result);
+        //     console.log(newProduct);
+        // } 
+        // else if (type === "Top") {
+        //     setProducts(newProduct);
+        //     result = newProduct.filter(product => product.category==="Top")
+        //     setProducts(result);
+        //     console.log(result);
+        //     console.log(newProduct);
+        // } 
+        // else if (type === "Bottom") {
+        //     setProducts(newProduct);
+        //     result = newProduct.filter(product => product.category==="Bottom")
+        //     setProducts(result);
+        //     console.log(result);
+        //     console.log(newProduct);
+        // } 
+        // else if (type === "Acc") {
+        //     setProducts(newProduct);
+        //     result = newProduct.filter(product => product.category.includes("Acc"))
+        //     setProducts(result);
+        //     console.log(result);
+        //     console.log(newProduct);
+        //     console.log(...products);
+        // } 
+    };
+
+    useEffect(() => {
         getProducts().then((data) => {
-          setProducts(data.data.products);
+        setProducts(data.data.products);
         });
-      }, [setProducts]);
+    }, [setProducts]);
 
     return ( 
         <div className="menu_header">
-            <div className="main_title">Exception;</div>
+            <div className="main_title" onClick={() => sortProduct("All")}>Exception;</div>
             <ul className="category">
                 <li><span onClick={() => sortProduct("All")}>All</span></li>
                 <li><span onClick={() => sortProduct("Outer")}>Outer</span></li>
