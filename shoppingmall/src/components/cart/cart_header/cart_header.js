9cdb295e179914ce5d551f2ca151bc043aaa5e48
geniuus <cross_man@naver.com>
2023-04-19 15:36:44 +0900

    Auto stash before merge of "main" and "origin/main"

--- shoppingmall/src/components/cart/cart_header/cart_header.js
+++ shoppingmall/src/components/cart/cart_header/cart_header.js
@@ -4,12 +4,20 @@ import Header from '../../header/header';
 export const CartHeader = ({ isAllSelected, allSelected}) => {
     return (
         <>
+<<<<<<< Updated upstream
             <div className="cart_tab">
                 <div className="checkbox_wrapper">
                     <input className="cart_checkbox" type="checkbox" checked={isAllSelected} onChange={(e) => allSelected(e.target.checked)} />
                 </div>
                 <h1 className="cart_title">My Cart</h1>
             </div>
+=======
+            <Header />
+            <div className="cart_tab">
+                <input className="cart_checkbox" type="checkbox" checked={isAllSelected} onChange={(e) => allSelected(e.target.checked)} />
+                <h1 className="cart_title">My Cart -</h1>
+           </div>
+>>>>>>> Stashed changes
         </>
     );
 };
